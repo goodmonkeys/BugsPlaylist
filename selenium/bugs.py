100613ca83b12a4117b011e7bef06f5d653834bc
Lee <goodmonkeys@naver.com>
2021-12-18 21:53:16 +0900

    2차원 배열로 저장 성공
    json 파일로 변환

--- selenium/bugs.py
+++ selenium/bugs.py
@@ -1,6 +1,10 @@
 # import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
 import urllib.request
@@ -37,59 +41,39 @@ try:
     elem = driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div/div/nav/ul/li[10]/div/a").click()
 
     time.sleep(5)
-    print("b\nb\nb\nb\n")
 
 # 로그인 완료
 
+    dic=[]
+    for title in WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "albumTitle"))):
+        stitle=[]
+        print("b\nb\nb\nb\n")
+        stitle.append(title.text)
+        print(title.text)
+        title.send_keys(Keys.CONTROL + "\n")        
+        driver.switch_to.window(driver.window_handles[1])
 
-    articlebox = driver.find_element_by_id('myAlbumListAjax')
-    articles = articlebox.find_elements_by_class_name('albumInfo')
-    title = articles[0].find_element_by_class_name('albumTitle')
-    print(title.text)
 
-    title.click()
+        time.sleep(2)
 
-    time.sleep(5)
+        for songtitle in WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "p.title"))):
+            # driver.implicitly_wait(10)
+            try:                
+                stitle.append(songtitle.text)
+                print(songtitle.text)
+            except:
+                pass
+                print("오류남 씨바꺼")
+                # 내 앨범에 노래가 0일 때도 오류남 이거 생각하기
 
-    titlebox = driver.find_element_by_css_selector('table.list.trackList')
-    songtitles = titlebox.find_elements_by_class_name('title')
-    for songtitle in songtitles:
-        try:
-            stitle = songtitle.find_element_by_tag_name('a').text
-            print(stitle)
-        except:
-            pass
-            print("오류남 씨바꺼")
+        dic.append(stitle)
 
-# 첫번째 앨범에서 제목 가져옴
+        driver.close()
 
-    driver.back()
-    time.sleep(2)
-
-    articlebox = driver.find_element_by_id('myAlbumListAjax')
-    articles = articlebox.find_elements_by_class_name('albumInfo')
-    title = articles[1].find_element_by_class_name('albumTitle')
-    
-    title.click()
-
-    time.sleep(5)
-
-    titlebox = driver.find_element_by_css_selector('table.list.trackList')
-    songtitles = titlebox.find_elements_by_class_name('title')
-    for songtitle in songtitles:
-        try:
-            stitle = songtitle.find_element_by_tag_name('a').text
-            print(stitle)
-        except:
-            pass
-            print("오류남 씨바꺼")
-
-# 두번째 일범에서 제목 가져옴
-
-    driver.back()
-    time.sleep(2)
-    
-
+        driver.switch_to.window(driver.window_handles[0])
+    print("출력을 시작학ㅆ음")  
+    for i in dic:
+        print(i)
 
 
 
@@ -99,41 +83,6 @@ except:
 
 
 
-
-
-# SCROLL_PAUSE_TIME = 1
-# # Get scroll height
-# last_height = driver.execute_script("return document.body.scrollHeight")
-# while True:
-#     # Scroll down to bottom
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     # Wait to load page
-#     time.sleep(SCROLL_PAUSE_TIME)
-#     # Calculate new scroll height and compare with last scroll height
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#     if new_height == last_height:
-#         try:
-#             driver.find_element_by_css_selector(".mye4qd").click()
-#         except:
-#             break
-#     last_height = new_height
-
-# images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-# count = 1
-# for image in images:
-#     try:
-#         image.click()
-#         time.sleep(2)
-#         imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
-#         opener=urllib.request.build_opener()
-#         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-#         urllib.request.install_opener(opener)
-#         urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-#         count = count + 1
-#     except:
-#         pass
-# 
-# driver.close()
 
 
 
